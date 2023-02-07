911c78fe05ca2464675679ff29e701abc57fc6bd
essam1212 <essam5ali2000@gmail.com>
2023-02-07 19:50:03 +0200

    commit 1

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const  mongoose  = require("mongoose");
+
+import { set } from "mongoose";
 require("dotenv").config();
 
-var cors = require('cors')
-const helmet = require("helmet");
-const allRoutes = require("./moduls/AllRouters")
+import cors from 'cors';
+import helmet from "helmet";
+import { authRouter, noteRouter } from "./moduls/AllRouters";
 
-const connect = require("./DB/connection");
-const express = require('express')
+import connect from "./DB/connection";
+import express, { json } from 'express';
 const app = express()
 app.use(helmet());
 
-app.use(express.json())
+app.use(json())
 app.use(cors())
 
 const port =process.env.PORT ;
 
-mongoose.set('strictQuery', true)
+set('strictQuery', true)
 connect();
 // app.use(allRoutes.authRouter, allRoutes.userRouter);
-app.use("/api/v1/auth", allRoutes.authRouter);
-app.use("/api/v1/note", allRoutes.noteRouter)
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/note", noteRouter)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen( port, () => console.log(`Example app listening on port ${port}!`))
