f5de837ebe828e0b6aae607c715fe89aa2f9a0ff
essam1212 <essam5ali2000@gmail.com>
2023-02-14 20:45:34 +0200

    commit 1

--- moduls/auth/auth.validation.js
+++ moduls/auth/auth.validation.js
@@ -2,16 +2,16 @@ import joi from 'joi'
 
 export const signUpValidation={
     body:joi.object().required().keys({
-        userName:joi.string().pattern(new RegExp(/^[A-Za-z]{3,8}/)).required(),
+        userName:joi.string().pattern(new RegExp(/^[A-Za-z]{3,15}/)).required(),
         email:joi.string().email().required().messages({ 'string.email': 'pleas enter real email' }),
-        password:joi.string().pattern(new RegExp(/^[A-Za-z0-9]{3,15}$/)).required().messages({ 'string.pattern.base': 'pleas enter password fails  to match the required pattern :3-15  ' }),
+        password:joi.string().pattern(new RegExp(/^[A-Za-z0-9]{3,15}$/)).required().messages({ 'string.pattern.base': 'pleas enter password fails  to match the required pattern :3-15' }),
         cpassword:joi.string().valid(joi.ref('password')).required(),
     })
 }
 export const loginValidation={
     body:joi.object().required().keys({
         email:joi.string().email().required(),
-        password:joi.string().pattern(new RegExp(/^[A-Za-z1-9]{3,15}$/)).required().messages({ 'string.pattern.base': 'pleas enter password fails  to match the required pattern :3-15 ' }),
+        password:joi.string().pattern(new RegExp(/^[A-Za-z0-9]{3,15}$/)).required().messages({ 'string.pattern.base': 'pleas enter password fails  to match the required pattern :3-15' }),
        
     })
 }
