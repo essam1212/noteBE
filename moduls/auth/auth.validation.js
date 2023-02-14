10af19144b3d5bbf2dd31dc7ebfb8518e3808c00
essam1212 <essam5ali2000@gmail.com>
2023-02-14 21:35:48 +0200

    v1

--- moduls/auth/auth.validation.js
+++ moduls/auth/auth.validation.js
@@ -3,7 +3,10 @@ import joi from 'joi'
 export const signUpValidation={
     body:joi.object().required().keys({
         userName:joi.string().pattern(new RegExp(/^[A-Za-z]{3,15}/)).required(),
-        email:joi.string().email().required().messages({ 'string.email': 'pleas enter real email' }),
+        email:joi.string().email().required().messages({
+            "any.required": "{{#label}} is required!!",
+            "string.email": "{{#label}} please enter rele email!!",
+          }),
         password:joi.string().pattern(new RegExp(/^[A-Za-z0-9]{3,15}$/)).required().messages({ 'string.pattern.base': 'pleas enter password fails  to match the required pattern :3-15' }),
         cpassword:joi.string().valid(joi.ref('password')).required(),
     })
